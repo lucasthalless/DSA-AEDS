170b1bf771b75ae2f8481bc75bd613a991b29474
lucasthalless <67612669+lucasthalless@users.noreply.github.com>
2026-05-19 17:31:26 -0300

    Create two-pointer-approach.ts

---
+++ minimum-common-value/two-pointer-approach.ts
@@ -0,0 +1,17 @@
+function getCommon(nums1: number[], nums2: number[]): number {
+
+    let currNum1 = 0
+    let currNum2 = 0
+
+    while (currNum1 < nums1.length || currNum2 < nums2.length) {
+        if (nums1[currNum1] == nums2[currNum2]) return nums1[currNum1]
+
+        if (nums1[currNum1] > nums2[currNum2] && currNum2 < nums2.length) {
+            currNum2 ++
+        } else {
+            currNum1 ++
+        }
+    }
+
+    return -1
+};
